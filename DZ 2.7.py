7059a85119679199025f95ba06e8323b5d8f10e8
Timurstopnik <128400221+Timurstopnik@users.noreply.github.com>
2023-05-08 12:21:35 +0300

    Add files via upload

---
+++ DZ 2.7.py
@@ -0,0 +1,7 @@
+a = float(input('Input first number: '))
+b = float(input('Input second nuber: '))
+list = [a,b]
+if a == b:
+    print('This numbers are equal')
+else:
+    print(sorted(list)) #С этой задачкой пришлось помучаться долго, и придумывать как сделать список из двух чисел хд
